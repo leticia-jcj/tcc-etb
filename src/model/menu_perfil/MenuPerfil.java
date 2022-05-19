e61f2ad73a6ca5f3deff0752b11179bf5a942496
visitante <visitante@host.docker.internal>
2022-05-18 22:46:30 -0300

    getMenuPerfil() feito

--- src/model/menu_perfil/MenuPerfil.java
+++ src/model/menu_perfil/MenuPerfil.java
@@ -2,9 +2,16 @@ package model.menu_perfil;
 
 import java.awt.Menu;
 import java.sql.Date;
-
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 import model.perfil.Perfil;
 
+@NoArgsConstructor
+@ToString
+@Getter
+@Setter
 public class MenuPerfil {
 	
     private int idMenuPerfil;
--- src/model/menu_perfil/MenuPerfilDAO.java
+++ src/model/menu_perfil/MenuPerfilDAO.java
@@ -1,7 +1,42 @@
 package model.menu_perfil;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import factory.ConexaoFactory;
+
 public class MenuPerfilDAO {
 
+	Connection con;
+	PreparedStatement ps;
+	ResultSet rs;
+	String sql;
+	
+	public MenuPerfil getMenuPerfil(int idMenuPerfil)throws 
+	SQLException {
+		MenuPerfil mp = new MenuPerfil();
+	sql = "SELECT idMenuPerfil, "
+			+ "dataCadastro, " +
+		  "FROM menuPerfil WHERE idMenuPerfil = ?";
+	
+	con = ConexaoFactory.conectar();
+	ps = con.prepareStatement(sql);
+	ps.setInt(1, idMenuPerfil);
+	rs = ps.executeQuery();
+	
+	if(rs.next()) {
+		mp.setIdMenuPerfil(rs.getInt("idMenuPerfil"));
+		mp.setDataCadastro(rs.getDate("dataCadastro"));
+	}
+	
+	ConexaoFactory.desconectar(con);
+	return mp;
+}
+
+	
+	
 	
 }
 
